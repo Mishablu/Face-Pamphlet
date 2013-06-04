3a101a182ef7225dc40a8cfc5b70a9f0b044cbc6
S <s>
2013-06-03 18:42:51 -0700

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -14,13 +14,44 @@ import javax.swing.*;
 public class FacePamphlet extends Program 
 					implements FacePamphletConstants {
 
+	private JTextField nameField;
+	private JTextField statusField;
+	private JTextField pictureField;
+	private JTextField friendField;
+	
 	/**
 	 * This method has the responsibility for initializing the 
 	 * interactors in the application, and taking care of any other 
 	 * initialization that needs to be performed.
 	 */
 	public void init() {
-		// You fill this in
+		
+		//add textfields and buttons to the NORTH
+		//add the name label to the north indicating that a name should be inserted into the text box
+		add(new JLabel("Name"), NORTH);
+		//add textfield to the north where the user will insert a name
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		add(nameField, NORTH);
+		//add the add, delete and lookup buttons
+		add(new JButton("Add"), NORTH);
+		add(new JButton("Delete"), NORTH);
+		add(new JButton("Lookup"), NORTH);
+		
+		//add textfields and buttons to the WEST
+		statusField = new JTextField(TEXT_FIELD_SIZE);
+		add(statusField, WEST);
+		add(new JButton("Change Status"));
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		
+		pictureField = new JTextField(TEXT_FIELD_SIZE);
+		add(pictureField, WEST);
+		add(new JButton("Change Picture"));
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		
+		friendField = new JTextField(TEXT_FIELD_SIZE);
+		add(friendField, WEST);
+		add(new JButton("Add Friend"));
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
     }
     
   
