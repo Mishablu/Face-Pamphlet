41f881477eff461d323f328e61b2d50cd680101f
S <s>
2013-06-05 02:46:25 -0700

    {"type":"TI", "runs":"1"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -60,8 +60,6 @@ public class FacePamphletCanvas extends GCanvas
 		messageLabel.setLabel(msg);
 		messageLabel.setFont(MESSAGE_FONT);
 		add(messageLabel, getWidth()/2-(messageLabel.getWidth()/2), getHeight()-BOTTOM_MESSAGE_MARGIN);
-		GLabel test = new GLabel ("test", 0, getHeight());
-		add (test);
 	}
 	
 	
